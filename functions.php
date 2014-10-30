1c06c10383138e344348c0d1f2b6e840a55d326b
Thomas <thomasflorentin@yahoo.fr>
2014-10-30 12:17:19 +0100

    Polish sidebar admin

--- functions.php
+++ functions.php
@@ -82,16 +82,7 @@ add_action( 'after_setup_theme', 'ipsofacto_setup' );
  */
 function ipsofacto_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'ipsofacto' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-	register_sidebar( array(
-		'name'          => __( 'Sidebar Accueil', 'ipsofacto' ),
+		'name'          => __( 'Contenus colonne Accueil', 'ipsofacto' ),
 		'id'            => 'sidebar-hp',
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
