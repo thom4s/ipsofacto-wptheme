2e17d56a43e565d086aa48c6bb596201b7f4db4e
Thomas <thomasflorentin@yahoo.fr>
2014-10-31 11:49:00 +0100

    Responsive navigation

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'ipsofacto' ); ?></button>
+			<button class="menu-toggle"><?php _e( 'Menu', 'ipsofacto' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 
